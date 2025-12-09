4de990a90b051225dde523f0e0b9eaf45fdcf893
MOHSIN-JAWADWALA <mohsinjawadwala8@gmail.com>
2025-12-09 22:21:30 +0530

    Build Partial Home Component

--- app/(app)/Home.js
+++ app/(app)/Home.js
@@ -1,18 +1,37 @@
-import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import ChatList from '../../components/ChatList';
 import { useAuth } from '../../Context/AuthContext';
 
 export default function Home() {
   const {logout , user} =useAuth();
   const [users,setUsers] =useState([1,2,3])
   
+  useEffect(()=>{
+    if(user?.uid)
+    getUsers();
+  },[])
 
-  console.log('user data :', user);
+  const getUsers =async () => {
+ // fetch users
+  }
   
   return (
-
     <View className='flex-1 bg-white'>
-     <Text className='text-black'>Home Screen</Text>
+      <StatusBar style="light"/>
+      {
+        users.length>0? (
+          <ChatList users={users} />
+        ):(
+          <View className="flex items-center " style={{top:hp(30)}} >
+            <ActivityIndicator size="large" />
+            
+          </View>
+        )
+      }
+     
     </View>
   )
 }
---
+++ components/ChatList.js
@@ -0,0 +1,22 @@
+import { useRouter } from 'expo-router'
+import { FlatList, View } from 'react-native'
+import ChatItem from './ChatItem'
+
+export default function ChatList({users}) {
+    const router = useRouter()
+  return (
+    <View className="flex-1">
+      <FlatList
+      data={users}
+      contentContainerStyle={{flex:1 , paddingVertical:25}}
+      keyExtractor={item=>Math.random()}
+      showsVerticalScrollIndicator={false}
+      renderItem={({item,index})=> <ChatItem 
+      noBorder={index+1 == users.lenght} 
+      router={router} 
+      item={item} 
+      index={index} />}
+      />
+    </View>
+  )
+}
